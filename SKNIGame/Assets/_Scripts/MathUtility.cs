5ae2892f7539d087e06efaf592c723abb1c319ca
Pawel <dziedzicpm@gmail.com>
2017-12-23 15:21:29 +0100

    Back to 17.3, gesture recognition - first iteration

---
+++ SKNIGame/Assets/_Scripts/MathUtility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathUtility
+{
+	///<Summary> Calculate distance between point and line passing through points A and B </Summary>
+	public static float DistPointToLine(Vector2 A, Vector2 B, Vector2 point)
+	{
+		return Mathf.Abs((point.x - A.x) * (-B.y + A.y) + (point.y - A.y) * (B.x - A.x)) / Mathf.Sqrt((-B.y + A.y) * (-B.y + A.y) + (B.x - A.x) * (B.x - A.x));
+	}
+}
