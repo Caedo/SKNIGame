c560e0eae7a2d94cfa42b2ef39f0cb0fd443c775
Pawel <dziedzicpm@gmail.com>
2018-02-20 22:10:57 +0100

    Tons of Particles

---
+++ SKNIGame/Assets/_Scripts/Visual Effects/ParticleProjectileTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleProjectileTester : MonoBehaviour {
+
+	public float m_Velocity = 10;
+	public float m_MaxXPosition = 10;
+	public float m_MinXPosition = -10;
+
+	private ParticleSystem currentParticles;
+
+	private void Awake() {
+		currentParticles = GetComponent<ParticleSystem>();
+	}
+
+	private void Update() {
+		transform.position += Vector3.right * m_Velocity * Time.deltaTime;
+
+		if (transform.position.x > m_MaxXPosition) {
+			transform.position = new Vector3(m_MinXPosition, transform.position.y, transform.position.z);
+		} else if (transform.position.x < m_MinXPosition) {
+			transform.position = new Vector3(m_MaxXPosition, transform.position.y, transform.position.z);
+		}
+
+	}
+}
