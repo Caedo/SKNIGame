3ce8012698c392d754433792ba7829fc63403e6d
Pawel <dziedzicpm@gmail.com>
2018-02-23 15:35:54 +0100

    Moar particles

---
+++ SKNIGame/Assets/_Scripts/Visual Effects/PillarFloatingAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarFloatingAnimation : MonoBehaviour {
+
+	public float m_FloatingSpeed;
+	public float m_Amplitude;
+
+	Vector3 m_StartPos;
+
+	private void Start() {
+		m_StartPos = transform.position;
+	}
+
+	private void Update() {
+		transform.position = m_StartPos + Mathf.Sin(Time.time * m_FloatingSpeed) * m_Amplitude * Vector3.up;
+	}
+}
--- SKNIGame/Assets/_Scripts/Visual Effects/PillarShardRotator.cs
+++ SKNIGame/Assets/_Scripts/Visual Effects/PillarShardRotator.cs
@@ -6,11 +6,12 @@ public class PillarShardRotator : MonoBehaviour {
 
 	public Transform m_PillarTransform;
 	public float m_RotationSpeed;
+	public float m_SelfRotationSpeed;
 
 	void LateUpdate() {
 
 		transform.RotateAround(m_PillarTransform.position, Vector3.up, m_RotationSpeed * Time.deltaTime);
-		transform.Rotate(Vector3.forward * 100 * Time.deltaTime);
+		transform.Rotate(Vector3.forward * m_SelfRotationSpeed * Time.deltaTime);
 	}
 
 }
