658f2d564ac6c226104a3479650bacc1fb0d9ef9
Paweł <dziedzicpm@gmail.com>
2018-03-06 13:59:47 +0100

    Book and mouse control

---
+++ SKNIGame/Assets/_Scripts/BookController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookController : MonoBehaviour {
+
+	public float m_CreationTime = 1f;
+
+	bool m_IsVisible;
+	Material m_DissolveMaterial;
+
+	float m_CreationSpeed;
+	float m_VisibilityPercent;
+	float m_TargetVisPercent;
+
+	private void Awake() {
+		m_DissolveMaterial = GetComponent<Renderer>().material;
+	}
+
+	private void Update() {
+		//ONLY FOR TEST - we want this in some input manager, but later
+		if (Input.GetMouseButtonDown(1)) {
+			ToggleVisibility();
+		}
+	}
+
+	public void ToggleVisibility() {
+		m_IsVisible = !m_IsVisible;
+		m_TargetVisPercent = m_IsVisible ? 1 : 0;
+
+		StopCoroutine(ChangeVisibility());
+		StartCoroutine(ChangeVisibility());
+	}
+
+	IEnumerator ChangeVisibility() {
+		float timer = 0f;
+		while (timer <= 1) {
+			m_VisibilityPercent = Mathf.Lerp(1 - m_TargetVisPercent, m_TargetVisPercent, timer);
+			timer += Time.deltaTime * m_CreationSpeed;
+
+			m_DissolveMaterial.SetFloat("_DissolveStrength", m_VisibilityPercent);
+
+			yield return null;
+		}
+
+		m_VisibilityPercent = m_TargetVisPercent;
+		m_DissolveMaterial.SetFloat("_DissolveStrength", m_VisibilityPercent);
+
+	}
+
+	private void OnValidate() {
+		m_CreationSpeed = 1 / m_CreationTime;
+	}
+}
---
+++ SKNIGame/Assets/_Scripts/Player Character/CharacterMouseControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMouseControl : MonoBehaviour {
+
+	public Vector2 m_MouseSensitivity;
+	public Vector2 m_VerticalClampMinMax;
+	public Transform m_Camera;
+
+	float m_Pitch;
+	
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;	
+	}
+	
+	void Update () {
+		Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+		transform.Rotate(Vector3.up * mouseDelta.x * m_MouseSensitivity.x);
+		
+		m_Pitch += mouseDelta.y * m_MouseSensitivity.y;
+		m_Pitch = Mathf.Clamp(m_Pitch, m_VerticalClampMinMax.x, m_VerticalClampMinMax.y);
+
+		Quaternion quat = Quaternion.AngleAxis(m_Pitch, Vector3.left);
+		m_Camera.localRotation = quat;
+	}
+}
