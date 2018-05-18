51f8f77b74e179f81f3d1e01eab27608e0726621
Paweł Dziedzic <blavikenn@gmail.com>
2018-05-18 10:28:26 +0200

    Changed DamageTester for mouse controll and fixed bug with Exception when OnPillarDestroy was raised on destroyed enemy

--- SKNIGame/Assets/_Scripts/Damage System/DamageTester.cs
+++ SKNIGame/Assets/_Scripts/Damage System/DamageTester.cs
@@ -2,28 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageTester : MonoBehaviour
-{
-
+public class DamageTester : MonoBehaviour {
 	public Element attackElement;
 	public float m_Damage;
 
-	private void Update()
-	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit))
-			{
-				Debug.Log(hit.collider.name);
-				LivingEntity entity = hit.collider.GetComponent<LivingEntity>();
+	Transform m_CamTransform;
 
-				if (entity)
-				{
-					entity.Damage(m_Damage, attackElement);
-				}
+	public LineRenderer m_Line;
+
+	private void Start() {
+		m_CamTransform = Camera.main.transform;
+		InputController.Instance.SubscribeEventHandler("CastSpellDown", Fire);
+	}
+
+	void Fire() {
+
+		Ray ray = new Ray(m_CamTransform.position, m_CamTransform.forward);
+		RaycastHit hit;
+		m_Line.SetPosition(0, m_CamTransform.position + Vector3.down * 0.5f);
+		m_Line.SetPosition(1, ray.origin + ray.direction * 200f);
+		if (Physics.Raycast(ray, out hit)) {
+			var health = hit.collider.GetComponent<EnemyHealh>();
+			if (health) {
+				health.Damage(m_Damage, attackElement);
 			}
+			m_Line.SetPosition(1, hit.point);
 		}
+
 	}
 }
--- SKNIGame/Assets/_Scripts/Enemy/EnemyController.cs
+++ SKNIGame/Assets/_Scripts/Enemy/EnemyController.cs
@@ -119,4 +119,8 @@ public class EnemyController : MonoBehaviour {
             FindTarget();
         }
     }
+
+    private void OnDestroy() {
+        PillarHealth.OnPillarDestroy -= OnPillarDestroyed;
+    }
 }
